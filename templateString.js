294d1b4a2d1bd6624b53879234ca82abdfc8e30d
Nazmul Hossain <nazmuljuit@gmail.com>
2021-07-26 19:57:53 +0600

    update template string

---
+++ templateString.js
@@ -0,0 +1,15 @@
+
+console.clear();
+
+var name = 'Nazmul Hossain';
+
+console.log(`My name is ${name}`);
+
+/*ANOTHER EXAMPLE*/
+
+var address = 'District:Dhaka,Thana:Muhammadpur.';
+
+console.log(`My address is ${address}`);
+
+
+
