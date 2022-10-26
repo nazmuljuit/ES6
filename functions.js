84ed7c9ee073cb760d50c69b33ff301cd47093c4
Nazmul Hossain <nazmul.hossain@shampanit.com>
2022-10-26 18:14:45 +0600

    update

--- functions.js
+++ functions.js
@@ -1,7 +1,19 @@
 //Arguments vs. Parameters
+//Sometimes, you can use the terms argument and parameter interchangeably. However, by definition, parameters are what you specify in the function declaration whereas the arguments are what you pass into the function.
 function add(x, y) {
     console.log(x+y);
     return x + y;
  }
  
  add(100,200);
+
+ //Setting JavaScript default parameters for a function
+
+ function say(message='Hi') {
+    console.log(message);
+}
+
+say(); // 'Hi'
+say(undefined); // 'Hi'
+say('Hello'); // 'Hello'
+ 
