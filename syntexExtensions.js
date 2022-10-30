c3de29cb31e5ce90e59e4b59f07773526fa2df37
Nazmul Hossain <nazmul.hossain@shampanit.com>
2022-10-30 15:49:32 +0600

    update

--- syntexExtensions.js
+++ syntexExtensions.js
@@ -1,1 +1,7 @@
 //Summary: in this tutorial, you will learn about the syntax extensions of the object literal in ES6 that make your code cleaner and more flexible.
+function createMachine(name, status) {
+    return {
+        name: name,
+        status: status
+    };
+}
